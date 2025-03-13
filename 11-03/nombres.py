88307d06b82d651e599d4765956095356cdad60a
Margarita220414 <margaritajordan12345@gmail.com>
2025-03-13 10:38:24 -0500

    with open

---
+++ 11-03/nombres.py
@@ -0,0 +1,19 @@
+from import name 
+
+lista =[]
+file="data.txt"
+
+if _name_=="_main_":
+    print("ejecutado en el modulo")
+    print(f"variable name={_name_}")
+else:
+    print("ejecutado fuera del modulo")
+    print(f"variable name={_name_}")
+
+
+from clave in datos.keys:
+    print(clave)
+    print(f"rta={clave} y el valor={datos[clave]}")
+    for valor in datos.values():
+        print(valor)
+        
