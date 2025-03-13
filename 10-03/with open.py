ff11bbdb04bb8706170e665efd616d32609ffb4a
Margarita220414 <margaritajordan12345@gmail.com>
2025-03-13 10:37:44 -0500

    Exposición con Código
    Grupo con Juan David Cristancho

---
+++ 10-03/with open.py
@@ -0,0 +1,4 @@
+with open ('juancho.txt' ,'r') as archivos:
+    #aqui puede trabajar con el archivo
+    contenido=archivos.read()
+    print (contenido)
---
+++ 10-03/with openn.py
@@ -0,0 +1,4 @@
+white open (´archivo.txt´ y ´modo´) as archivos:
+    #aqui puede trabajar con el archivo
+    contenido=archivo.read()
+    print (contenido)
